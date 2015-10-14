ffc27d6ebe3af61ec8621ccba196f8b342500c68
Matthew <matthew.obrien929@gmail.com>
2015-10-14 15:07:35 -0700

    added test for conditions

---
+++ code/test/createcondition.c
@@ -0,0 +1,31 @@
+/* createcondition.c
+ *	Simple program to test whether running a user program works.
+ *	
+ *	Testing the creation of a condition variable
+ *
+ * 	NOTE: for some reason, user programs with global data structures 
+ *	sometimes haven't worked in the Nachos environment.  So be careful
+ *	out there!  One option is to allocate data structures as 
+ * 	automatics within a procedure, but if you do this, you have to
+ *	be careful to allocate a big enough stack to hold the automatics!
+ */
+
+#include "syscall.h"
+
+int
+main()
+{
+    int l = CreateLock("abc",3);
+    Acquire(l);
+
+    int c = CreateConditionSyscall("abc", 3);
+    SignalSyscall(l, c);
+    // BroadcastSyscall(l, c);
+    // WaitSyscall(l, c);
+
+    Release(l);
+    DestroyCondition(c);
+    DestroyLock(l);
+
+    Exit()
+}
