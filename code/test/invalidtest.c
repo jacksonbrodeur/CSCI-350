04dd8c952ecd9de761112282afa435846d80d619
Jackson Brodeur <jacksonbrodeur@gmail.com>
2015-10-15 16:56:41 -0700

    fixed invalidtest

--- code/test/invalidtest.c
+++ code/test/invalidtest.c
@@ -22,27 +22,29 @@ int main()
 		Only in create lock/condition do we check anything else - MAXFILENAME (=256) & bad vaddr
 	*/
 
-	// Locks - Create
-	int l1 = CreateLock("l1", -1); // below range
-	int l2 = CreateLock("l2", 300); // above range
-	/* code to test bad vaddr */ // can we test bad vaddr?
+	/* Locks - Create */
+	int l1 = CreateLock("l1", -1); /* below range */
+	int l2 = CreateLock("l2", 300); /* above range */
+	/* code to test bad vaddr */ /* can we test bad vaddr? */
 
-	// Locks - the rest
-	DestroyLock(-1); // below range
-	DestroyLock(100001); // above range
-	DestroyLock(9999); // lock should be NULL
-	// diff addr space (exec)
+	/* Conditions - Create */
+    int c1 = CreateCondition("c1", -1); /* below range */
+	int c2 = CreateCondition("c2", 300); /* above range */
+	int c3 = CreateCondition("", 0); /* is this valid? */
+
+	/* Locks - the rest */
+	DestroyLock(-1); /* below range */
+	DestroyLock(100001); /* above range */
+	DestroyLock(9999); /* lock should be NULL */
+	/* diff addr space (exec) */
 
 
-	// Conditions - Create
-    int c1 = CreateCondition("c1", -1); // below range
-	int c2 = CreateCondition("c2", 300); // above range
-	int c3 = CreateCondition("", 0); // is this valid?
-	// Conditions - the rest
-	DestroyCondition(-1); // below range
-	DestroyCondition(100001); // above range
-	DestroyCondition(9999); // condition should be NULL
-	// diff addr space (exec)
+	
+	/* Conditions - the rest */
+	DestroyCondition(-1); /* below range */
+	DestroyCondition(100001); /* above range */
+	DestroyCondition(9999); /* condition should be NULL */
+	/* diff addr space (exec) */
 
 
     Exit(0);
