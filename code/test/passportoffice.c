7b57fbfb84179f117d4cc8c8026406fc8a10f38a
Dylan Davis <dylandavis@Dylans-MacBook-Pro.local>
2015-10-15 18:42:24 -0700

    added passport office skeleton

---
+++ code/test/passportoffice.c
@@ -0,0 +1,72 @@
+
+#include "syscall.h"
+int a[3];
+int b, c;
+
+struct Clerk {
+    
+};
+
+struct Customer {
+    
+};
+
+
+void pictureTransaction() {
+    
+    
+}
+
+void pictureClerk() {
+
+
+}
+
+void applicationTransaction() {
+    
+    
+}
+
+void applicationClerk() {
+    
+
+}
+
+void passportTransaction() {
+    
+   
+}
+
+void passportClerk() {
+    
+
+}
+
+void cashierTransaction() {
+    
+    
+}
+
+void cashier() {
+    
+  
+}
+
+int getInShortestLine() {
+    
+}
+
+void customer() {
+    
+}
+
+void manager() {
+    
+}
+
+
+
+int main()
+{
+    Yield();
+}
