43f12c8b34b49182d3ebd5a6b52f00f5eaae97ca
yglazko <yglazko@guest-wireless-207-151-035-050.usc.edu>
2015-11-30 15:26:55 -0800

    pic clerk conversion

--- code/test/pictureclerk.c
+++ code/test/pictureclerk.c
@@ -15,93 +15,94 @@ main()
     int bribeLineCondition;
     int clerkCondition;
 
-    int breakLock;
-    int breakCondition;
+    int breakLock = Get(picClerkLock, myLine);
+    int breakCondition = Get(picBribeLineCV, myLinke);
 
-    int clerkLock;
+    int clerkLock = Get(picClerkLock, myLine);
     int clerkType;
 
     Customer * customer;
     int money;
 
     Acquire(counterLock);
-    myLine = CreateSyscall(numPictureClerks);
+   
     SetSyscall(0);
     me = &pictureClerks[myLine];
     Release(counterLock);
     
-    while(GetSyscall(customersFinished) < NUM_CUSTOMERS + NUM_SENATORS) {
+    while(Get(customersFinished, 0) < NUM_CUSTOMERS + NUM_SENATORS) {
         Acquire(pictureClerkLock);
         
         /* If there is a customer in line signal him to the counter */
-        if(GetSyscall(picBribeLineCount, myLine) > 0) {
+        if(Get(picBribeLineCount, myLine) > 0) {
             Print("PictureClerk %i has signalled a customer to come to their counter\n", 67, myLine * 1000, 0);
             Signal(me->bribeLineCondition, pictureClerkLock);
-            SetSyscall(picState) = BUSY;
-        } else if(GetSyscall(picLineCount, myLine) > 0) {
+            Set(picState, myLine, BUSY);
+        } else if(Get(picLineCount, myLine) > 0) {
             Print("PictureClerk %i has signalled a customer to come to their counter\n", 67, myLine * 1000, 0);
             Signal(me->lineCondition, pictureClerkLock);
-            SetSyscall(picState) = BUSY;
+            Set(picState, myLine, BUSY);
         } else {
             
-            if(storeJustOpened >= NUM_CLERKS * 4) {
+            if(Get(storeJustOpened,0) >= NUM_CLERKS * 4) {
                 Print("PictureClerk %i is going on break\n", 35, myLine * 1000, 0);
                 Release(pictureClerkLock);
-                Acquire(me->breakLock);
-                me->state = ONBREAK;
+                Acquire(breakLock);
+                Set(picState, myLine, ONBREAK);
                 
-                if(me->state == ONBREAK) {
-                    Wait(me->breakCondition, me->breakLock);
+                if(Get(picState, myLine, ONBREAK)) {
+                    Wait(breakCondition,breakLock);
                 }
                 
                 Print("PictureClerk %i is coming off break\n", 37, myLine * 1000, 0);
-                Release(me->breakLock);
+                Release(breakLock);
                 Acquire(pictureClerkLock);
                 
-                if(me->bribeLineCount > 0) {
+                if(Get(picBribeLineCount, myLine) > 0) {
                     Print("PictureClerk %i has signalled a customer to come to their counter\n", 67, myLine * 1000, 0);
-                    Signal(me->bribeLineCondition, pictureClerkLock);
-                    me->state = BUSY;
-                } else if(me->lineCount > 0) {
+                    Signal(bribeLineCondition, pictureClerkLock);
+                    Set(picState, myLine, BUSY);
+                } else if(Get(piclineCount, myLine) > 0) {
                     Print("PictureClerk %i has signalled a customer to come to their counter\n", 67, myLine * 1000, 0);
-                    Signal(me->lineCondition, pictureClerkLock);
-                    me->state = BUSY;
+                    Signal(lineCondition, pictureClerkLock);
+                    Set(picState, myLine, BUSY);
                 }
                 else
-                    me->state = AVAILABLE;
+                    Set(picState, myLine, AVAILABLE);
                 
             } else {
-                me->state = AVAILABLE;
-                storeJustOpened++;
+                Set(picState, myLine, AVAILABLE);
+                Set(storeJustOpened, 0, Get(storeJustOpened,0));
             }
         }
         
-        Acquire(me->clerkLock);
+        Acquire(clerkLock);
         Release(pictureClerkLock);
         
-        Wait(me->clerkCondition, me->clerkLock);
-        Print("PictureClerk %i has received SSN from Customer %i\n", 51, myLine * 1000 + me->customer->id, 0);
+        Wait(clerkCondition, clerkLock);
+        int customerID = Get(picCustomer, myLine);
+        Print("PictureClerk %i has received SSN from Customer %i\n", 51, myLine * 1000 + customerID, 0);
         
-        while(me->customer->pictureTaken == 0) {
+        while(Get(picCustomer)->pictureTaken == 0) {
             
             if(firstTime != 1) {
                 Print("PictureClerk %i has been told that Customer %i does not like their picture\n", 76, myLine * 1000 + me->customer->id, 0);
             }
-            Print("PictureClerk %i has taken a picture of Customer %i\n", 52, myLine * 1000 + me->customer->id, 0);
-            Signal(me->clerkCondition, me->clerkLock);
-            Wait(me->clerkCondition, me->clerkLock);
+            Print("PictureClerk %i has taken a picture of Customer %i\n", 52, myLine * 1000 + customerID, 0);
+            Signal(clerkCondition, clerkLock);
+            Wait(clerkCondition, clerkLock);
             firstTime = 0;
         }
         
-        Print("PictureClerk %i has been told that Customer %i does like their picture\n", 72, myLine * 1000 + me->customer->id, 0);
+        Print("PictureClerk %i has been told that Customer %i does like their picture\n", 72, myLine * 1000 + customerID, 0);
         
         for(i = 0; i < j; i++) {
             Yield();
         }
         
-        me->customer->pictureFiled = 1;
-        Signal(me->clerkCondition, me->clerkLock);
-        Release(me->clerkLock);
+        Get(pictureFiled, customerID, true);
+        Signal(clerkCondition, clerkLock);
+        Release(clerkLock);
     }
     Exit(0);
     
