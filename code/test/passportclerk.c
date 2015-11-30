49fe84601df62697fcedafe2cf8f9cc78f214275
Dylan Davis <dylandavis@Dylans-MacBook-Pro.local>
2015-11-30 14:56:50 -0800

    converted passport clerk

--- code/test/passportclerk.c
+++ code/test/passportclerk.c
@@ -1,103 +1,104 @@
 #include "syscall.h"
+#include "setup.h"
 
 #define NUM_CUSTOMERS 20
 #define NUM_SENATORS 10
-
-
 
 main()
 {
     int myLine;
     int i;
-    int counterLock = GetSyscall(/*counterLock index*/);
-
-    int lineCount;
-    int bribeLineCount;
-    int state;
-    int lineCondition;
-    int bribeLineCondition;
-    int clerkCondition;
     
-    int breakLock;
-    int breakCondition;
-    
-    int clerkLock;
     int clerkType;
-    Customer * customer;
     int money;
     
     Acquire(counterLock);
-    myLine = CreateSyscall(/*numPassportClerks*/);
-    SetSyscall(/*numPassportClerks++*/);
-    me = &passportClerks[myLine];
+    myLine = Get(numPassportClerks, 0);
+    Set(numPassportClerks, 0, myLine + 1);
     Release(counterLock);
     
+    int lineCondition = Get(ppLineCV, myLine);
+    int bribeLineCondition = Get(ppBribeLineCV, myLine);
+    
+    int clerkCondition = Get(ppClerkCV, 0);
+    
+    int breakLock = Get(ppBreakLock, myLine);
+    int breakCondition = Get(ppBreakCV, myLine);
+    
+    int clerkLock = Get(ppClerkLock, myLine);
+
+    
     /* On duty while there are still customers who haven't completed process */
-    while(GetSyscall(/*customersFinished*/) < NUM_CUSTOMERS + NUM_SENATORS) {
+    while(Get(customersFinished, 0) < NUM_SENATORS + NUM_CUSTOMERS) {
+        
         Acquire(passportClerkLock);
         
         /* If there is a customer in line signal him to the counter */
-        if(GetSyscall(/*me->bribeLineCount*/) > 0) { /* Check bribe line first... */
+        if(Get(ppBribeLineCount,myLine) > 0) { /* Check bribe line first... */
             Print("Passport Clerk %i has signalled a customer to come to their counter\n", 69, myLine * 1000, 0);
-            Signal(me->bribeLineCondition, passportClerkLock);
-            SetSyscall(/*me->state*/) = BUSY;
-        } else if(GetSyscall(/*me->lineCount*/) > 0) { /* then check regular line */
+            Signal(bribeLineCondition, passportClerkLock);
+            Set(ppState,myLine,BUSY);
+        } else if(Get(ppLineCount,myLine) > 0) { /* then check regular line */
             Print("Passport Clerk %i has signalled a customer to come to their counter\n", 69, myLine * 1000, 0);
-            Signal(me->lineCondition, passportClerkLock);
-            me->state = BUSY;
+            Signal(lineCondition, passportClerkLock);
+            Set(ppState,myLine,BUSY);
         } else {
             
-            if(storeJustOpened >= NUM_CLERKS * 4) {
+            if(Get(storeJustOpened,0) >= NUM_CLERKS * 4) {
                 Print("Passport Clerk %i is going on break\n", 37, myLine * 1000, 0);
                 Release(passportClerkLock);
-                Acquire(me->breakLock);
-                me->state = ONBREAK;
+                Acquire(breakLock);
+                Set(ppState,myLine,ONBREAK);
                 
                 /* Mesa style monitor, wait to be woken up by manager */
-                if(me->state == ONBREAK) {
-                    Wait(me->breakCondition, me->breakLock);
+                if(Get(ppState,myLine,ONBREAK)) {
+                    Wait(breakCondition, breakLock);
                 }
                 
                 Print("Passport Clerk %i is coming off break\n", 39, myLine * 1000, 0);
-                Release(me->breakLock);
+                Release(breakLock);
                 Acquire(passportClerkLock);
                 
                 /* Tell the next customer in line that clerk is ready */
-                if(me->bribeLineCount > 0) { /* Check bribe line first... */
+                if(Get(ppBribeLineCount,myLine) > 0) { /* Check bribe line first... */
                     Print("Passport Clerk %i has signalled a customer to come to their counter\n", 69, myLine * 1000, 0);
-                    Signal(me->bribeLineCondition, passportClerkLock);
-                    me->state = BUSY;
-                } else if(me->lineCount > 0) { /* then check regular line */
+                    Signal(bribeLineCondition, passportClerkLock);
+                    Set(ppState,myLine,BUSY);
+                } else if(Get(ppLineCount,myLine) > 0) { /* then check regular line */
                     Print("Passport Clerk %i has signalled a customer to come to their counter\n", 69, myLine * 1000, 0);
-                    Signal(me->lineCondition, passportClerkLock);
-                    me->state = BUSY;
+                    Signal(lineCondition, passportClerkLock);
+                    Set(ppState,myLine,BUSY);
                 }
                 else
-                    me->state = AVAILABLE;
+                    Set(ppState,myLine,AVAILABLE);
             }
             else {
-                me->state=AVAILABLE;
-                storeJustOpened++;
+                Set(ppState,myLine,AVAILABLE);
+                Set(storeJustOpened,0,Get(storeJustOpened,0));
             }
         }
         
         /* entering transaction so switch locks */
-        Acquire(me->clerkLock);
+        Acquire(clerkLock); //TODO: These acquire/release statements may need to be reversed
         Release(passportClerkLock);
         
         /* wait for customer data */
-        Wait(me->clerkCondition, me->clerkLock);
+        Wait(clerkCondition, clerkLock);
         
         
-        Print("Passport Clerk %i has received SSN from Customer %i\n", 53, myLine * 1000 + me->customer->id, 0);
-        Print("Passport Clerk %i has determined that Customer %i does have both their application and picture completed\n", 106, myLine * 1000 + me->customer->id, 0);
+        int customerID = Get(ppCustomer,myLine);
+        
+        Print("Passport Clerk %i has received SSN from Customer %i\n", 53, myLine * 1000 + customerID, 0);
+        Print("Passport Clerk %i has determined that Customer %i does have both their application and picture completed\n", 106, myLine * 1000 + customerID, 0);
+        
+        
         for(i = 0; i < Rand()% 80 + 20; i++) {
             Yield();
         }
-        me->customer->passportCertified = TRUE;
-        Print("Passport Clerk %i has recorded Customer %i passport documentation\n", 67, myLine * 1000 + me->customer->id, 0);
-        Signal(me->clerkCondition, me->clerkLock);
-        Release(me->clerkLock);
+        Set(passportCertified,customerID,TRUE);
+        Print("Passport Clerk %i has recorded Customer %i passport documentation\n", 67, myLine * 1000 + customerID, 0);
+        Signal(clerkCondition, clerkLock);
+        Release(clerkLock);
     }
     Exit(0);
 
