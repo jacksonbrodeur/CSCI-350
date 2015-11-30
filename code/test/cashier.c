3c8be56cd1b52e711cc815e5e916cc538b94cd26
Dylan Davis <dylandavis@Dylans-MacBook-Pro.local>
2015-11-30 15:10:37 -0800

    converted cashier

--- code/test/cashier.c
+++ code/test/cashier.c
@@ -2,74 +2,84 @@
 
 main()
 {
-    Clerk tempClerk;
-    Clerk * me;
     int myLine;
     
     Acquire(counterLock);
-    myLine = numCashiers;
-    numCashiers++;
-    me = &cashiers[myLine];
+    myLine = Get(numCashiers,0);
+    Set(numCashiers,0,myLine+1);
     Release(counterLock);
     
+    int lineCondition = Get(cashLineCV,myLine);
+    int bribeLineCondition = Get(cashBribeLineCV,myLine);
+    int clerkCondition = Get(cashClerkCV,myLine);
+    
+    int breakLock = Get(cashBreakLock,myLine);
+    int breakCondition = Get(cashBreakCV,myLine);
+    
+    int clerkLock = Get(cashClerkLock,myLine);
+    
+    int cashMoney;
+    
     /* On duty while there are still customers who haven't completed process */
-    while(customersFinished < NUM_CUSTOMERS + NUM_SENATORS) {
+    while(Get(customersFinished,0) < NUM_CUSTOMERS + NUM_SENATORS) {
         Acquire(cashierLock);
         
-        if (me->lineCount > 0) {
+        if (Get(lineCount,myLine) > 0) {
             Print("Cashier %i has signalled a customer to come to their counter\n", 62, myLine * 1000, 0);
-            Signal(me->lineCondition, cashierLock);
-            me->state = BUSY;
+            Signal(lineCondition, cashierLock);
+            Set(cashState,myLine,BUSY);
         } else {
-            if(storeJustOpened>=NUM_CLERKS*4) {
+            if(Get(storeJustOpened,0)>=NUM_CLERKS*4) {
                 Print("Cashier %i is going on break\n", 30, myLine * 1000, 0);
                 Release(cashierLock);
-                Acquire(me->breakLock);
-                me->state = ONBREAK;
+                Acquire(breakLock);
+                Set(cashState,myLine,ONBREAK);
                 
                 /* Mesa style monitor, wait to be woken up by manager */
-                if(me->state == ONBREAK) {
-                    Wait(me->breakCondition, me->breakLock);
+                if(Get(cashState,myLine) == ONBREAK) {
+                    Wait(breakCondition, breakLock);
                 }
                 
                 Print("Cashier %i is coming off break\n", 32, myLine * 1000, 0);
-                Release(me->breakLock);
+                Release(breakLock);
                 Acquire(cashierLock);
                 
-                if (me->lineCount > 0) {
+                if (Get(lineCount,myLine) > 0) {
                     Print("Cashier %i has signalled a customer to come to their counter\n", 62, myLine * 1000, 0);
-                    Signal(me->lineCondition, cashierLock);
-                    me->state = BUSY;
+                    Signal(lineCondition, cashierLock);
+                    Set(cashState,myLine,BUSY);
                 }
                 else
-                    me->state = AVAILABLE;
+                    Set(cashState,myLine,AVAILABLE);
             }
             else {
-                me->state = AVAILABLE;
-                storeJustOpened++;
+                Set(cashState,myLine,AVAILABLE);
+                Set(storeJustOpened,0,Get(storeJustOpened,0)+1);
             }
         }
         
         /* entering transaction so switch locks */
-        Acquire(me->clerkLock);
+        Acquire(clerkLock);
         Release(cashierLock);
         
         /* wait for customer to pay */
-        Wait(me->clerkCondition, me->clerkLock);
+        Wait(clerkCondition, clerkLock);
         
-        Print("Cashier %i has received SSN from Customer %i\n", 46, myLine * 1000 + me->customer->id, 0);
-        Print("Cashier %i has verified that Customer %i has been certified by a PassportClerk\n", 80, myLine * 1000 + me->customer->id, 0);
+        int customerID = Get(cashCustomer,myLine);
+        
+        Print("Cashier %i has received SSN from Customer %i\n", 46, myLine * 1000 + customerID, 0);
+        Print("Cashier %i has verified that Customer %i has been certified by a PassportClerk\n", 80, myLine * 1000 + customerID, 0);
         /* taking payment */
-        me->customer->money -= 100;
-        me->money += 100;
-        Print("Cashier %i has received the $100 from Customer %i after certification\n", 71, myLine * 1000 + me->customer->id, 0);
+        Set(custMoney,customerID, Get(custMoney,customerID)-100);
+        Set(cashMoney,myLine,Get(cashMoney,myLine)+100);
+        Print("Cashier %i has received the $100 from Customer %i after certification\n", 71, myLine * 1000 + customerID, 0);
         
-        me->customer->passportGiven = TRUE;
-        Signal(me->clerkCondition, me->clerkLock);
-        Print("Cashier %i has provided Customer %i their completed passport\n", 62, myLine * 1000 + me->customer->id, 0);
-        Print("Cashier %i has recorded that Customer %i has been given their completed passport\n", 82, myLine * 1000 + me->customer->id, 0);
+        Set(passportGiven,customerID, TRUE);
+        Signal(clerkCondition, clerkLock);
+        Print("Cashier %i has provided Customer %i their completed passport\n", 62, myLine * 1000 + customerID, 0);
+        Print("Cashier %i has recorded that Customer %i has been given their completed passport\n", 82, myLine * 1000 + customerID, 0);
         
-        Release(me->clerkLock);
+        Release(clerkLock);
     }   
     Exit(0);
 
